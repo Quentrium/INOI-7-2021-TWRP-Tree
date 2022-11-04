7618163af370fa9a602a3068cdd4248ace51ee15
Quentrium <116377715+Quentrium@users.noreply.github.com>
2022-11-04 12:54:07 +0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_INOI_7_4_64_2021.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_INOI_7_4_64_2021-user \
+    omni_INOI_7_4_64_2021-userdebug \
+    omni_INOI_7_4_64_2021-eng
